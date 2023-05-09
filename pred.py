8b0e79791bb637bc0e64cb9a90eedb96d3960f6e
Abdul Haseeb <mohammedabdulhaseeb4@gmail.com>
2023-05-09 17:10:14 +0530

    Added shell file

--- pred.py
+++ pred.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
-
+CORS(app)
 # # Load the KNN model from a joblib file
 knn_saved = joblib.load('knn_har_model.joblib')
 
